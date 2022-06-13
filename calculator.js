2465d81a3f1e453f6815547cb662a9c88202579e
toshiroto <chtopete@gmail.com>
2022-06-13 16:23:50 +0900

    simple calculator done

--- calculator.js
+++ calculator.js
@@ -1,11 +1,22 @@
 //jshint-esversion:6
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express()
+app.use(bodyParser.urlencoded({extended: true}));
 
 const port = 3000
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/index.html")
+})
+app.post('/', (req, res) => {
+
+var num1 = Number(req.body.n1);
+var num2 = Number(req.body.n2);
+
+var result = num1 + num2;
+
+res.send("The result of the calculation is" +" "+ result)
 })
 
 app.listen(port, () => {
